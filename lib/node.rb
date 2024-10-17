9e833874dc3509403b1936bc34cf4517d3bfac52
jose delgado <j2delgadob@gmail.com>
2024-10-17 16:26:14 -0500

    feat: add partition simulation

--- lib/node.rb
+++ lib/node.rb
@@ -1,5 +1,5 @@
 class Node
-  attr_accessor :id, :neighbors, :log, :accepted_states, :proposed_state, :leader, :votes, :term, :voted_for
+  attr_accessor :id, :neighbors, :log, :accepted_states, :proposed_state, :leader, :votes, :term, :voted_for, :partitioned_nodes
 
   def initialize(id)
     @id = id
@@ -11,6 +11,7 @@ class Node
     @votes = 0
     @term = 0
     @voted_for = nil
+    @partitioned_nodes = []
   end
 
   def add_neighbor(node)
@@ -19,10 +20,22 @@ class Node
 
   def become_leader
     @leader = true
-    log << "Node #{@id} became leader"
+    log << "Node #{@id} became leader in term #{@term}"
   end
 
   def start_election
+    if @leader
+      log << "Node #{@id} is already a leader in term #{@term}, no need to start a new election."
+      return
+    end
+
+    neighbors.each do |neighbor|
+      if neighbor.leader
+        log << "Node #{@id} canceled its election for term #{@term} because Node #{neighbor.id} is already the leader."
+        return
+      end
+    end
+
     @term += 1
     @voted_for = self
     @votes = 1
@@ -32,18 +45,22 @@ class Node
 
   def send_vote_request
     neighbors.each do |neighbor|
-      neighbor.receive_vote_request(self, @term)
+      unless partitioned_nodes.include?(neighbor)
+        neighbor.receive_vote_request(self, @term)
+      else
+        log << "Node #{@id} could not send vote request to Node #{neighbor.id} due to partition"
+      end
     end
   end
 
   def receive_vote_request(candidate, candidate_term)
-    if candidate_term > @term && @voted_for.nil?
+    if (candidate_term > @term || candidate_term == @term) && @voted_for.nil?
       @term = candidate_term
       @voted_for = candidate
       log << "Node #{@id} voted for Node #{candidate.id} in term #{candidate_term}"
       candidate.receive_leader_vote(self)
     else
-      log << "Node #{@id} rejected vote request from Node #{candidate.id}"
+      log << "Node #{@id} rejected vote request from Node #{candidate.id} for term #{candidate_term}"
     end
   end
 
@@ -65,30 +82,42 @@ class Node
 
   def propose_state(state)
     if @leader
-      @propose_state = state
+      @proposed_state = state
       log << "Node #{@id} (leader) proposes state: #{state}"
-      accepted_states << state
       @votes = 1
       accepted_states << state
       send_proposal(state)
     else
-      log << "Node #{@id} tried to propose state but is not the leader"
+      log << "Node #{@id} tried to propose state #{state} but is not the leader"
     end
   end
 
   def send_proposal(state)
     neighbors.each do |neighbor|
-      neighbor.receive_proposal(state, self)
+      unless partitioned_nodes.include?(neighbor)
+        neighbor.receive_proposal(state, self)
+      else
+        log << "Node #{@id} could not send proposal to Node #{neighbor.id} due to partition"
+      end
     end
   end
 
   def receive_proposal(state, leader)
-    log << "Node #{@id} received state: #{state} from Node #{leader.id} (leader)"
-    accepted_states << state
-    leader.receive_vote(self, state)
+    unless partitioned_nodes.include?(leader)
+      log << "Node #{@id} received state: #{state} from Node #{leader.id} (leader)"
+      accepted_states << state
+      leader.receive_state_vote(self, state)
+    else
+      log << "Node #{@id} could not receive proposal from Node #{leader.id} due to partition"
+    end
   end
 
-  def receive_vote(neighbor, state)
+  def receive_state_vote(neighbor, state = nil)
+    majority = ((neighbors.size + 1) / 2.0).ceil
+    if @votes >= majority
+      log << "Node #{@id} already reached consensus on state #{@proposed_state} (Votes: #{@votes})"
+      return
+    end
     if state == @proposed_state
       @votes += 1
       log << "Node #{@id} received vote from Node #{neighbor.id} for state #{state}"
@@ -97,12 +126,17 @@ class Node
   end
 
   def check_consensus
-    majority = ((neighbors.size + 1) / 2.0).ceil 
+    majority = ((neighbors.size + 1) / 2.0).ceil
     if @votes >= majority
-      log << "Consensus reached on state: #{@proposed_state}"
+      log << "Consensus reached on state: #{@proposed_state} (Votes: #{@votes})"
     else
       log << "Consensus not yet reached on state: #{@proposed_state} (Votes: #{@votes})"
     end
+  end
+
+  def simulate_partition(partitioned_nodes)
+    @partitioned_nodes = partitioned_nodes
+    log << "Node #{@id} is partitioned from #{partitioned_nodes.map(&:id).join(', ')}"
   end
 
   def retrieve_log
--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -13,16 +13,14 @@ RSpec.describe Node do
     @node2.add_neighbor(@node3)
     @node3.add_neighbor(@node1)
     @node3.add_neighbor(@node2)
-  
-  end
-
-  it 'start election' do
     @node1.start_election
   end
 
-  it 'proposes a state' do
+  it 'proposes a state and simulate partition' do
     @node1.propose_state(1)
     @node2.propose_state(2)
+    @node3.simulate_partition([@node1])
+    @node2.propose_state(3)
     puts @node1.retrieve_log
     puts @node2.retrieve_log
     puts @node3.retrieve_log
